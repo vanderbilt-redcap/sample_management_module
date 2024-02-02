5ab4ec2b95143e80ca65c58bf3b111cde7cca09e
moorejr5 <james.r.moore@vumc.org>
2024-02-01 23:13:06 -0600

    Fix for check-in/check-out process.

--- js/functions.js
+++ js/functions.js
@@ -141,7 +141,7 @@ function loadContainer(ajaxurl,project_id,event_id,record,table_id,container = '
             }
             tableHTML += "<td><span class='slot_label'>" + slotLabel + "</span>";
             if (value['sample_id'] != "") {
-                tableHTML += "<div id='sample_slot_" + slotID + "'>Part. ID: " + value['participant_id'] + "<br/>Samp. ID: " + value['sample_id'] + "<br/>Sample Type: " + value['planned_type'] + "<br/>Collect Date: " + value['collect_date'] + "<br/><input value='Checkout' type='button' id='sample_checkout_" + slotLabel + "' onclick='checkoutSample(\""+ajaxurl+"\",\""+project_id+"\",\""+value['sample_id']+"\",\""+slotID+"\");' /></div>";
+                tableHTML += "<div id='sample_slot_" + slotID + "'>Part. ID: " + value['participant_id'] + "<br/>Samp. ID: " + value['sample_id'] + "<br/>Sample Type: " + value['planned_type'] + "<br/>Collect Date: " + value['collect_date'] + "<br/><input value='Checkout' type='button' id='sample_checkout_" + slotLabel + "' onclick='checkoutSample(\""+ajaxurl+"\",\""+project_id+"\",\""+event_id+"\",\""+value['sample_id']+"\",\""+slotID+"\",\""+slotLabel+"\",\"barcode_slot_"+value['project_id'] + "_" + value['record'] + "_" + value['event'] + "_" + (value['instance']+1)+"\");' /></div>";
             } else {
                 tableHTML += "<div id='sample_slot_" + slotID + "'><span class='scan_barcode'><label for='barcode_slot_" + slotID + "'>Scan Barcode:</label><input class='barcode_text' type='text' id='barcode_slot_" + slotID + "' oninput='saveSample(\""+ajaxurl+"\",\""+project_id+"\",\""+event_id+"\",this.value,\"" + slotID + "\",\"" + slotLabel + "\",\"sample_issue_\",\"container_select\");loadSample(\""+ajaxurl+"\",\""+project_id+"\",\""+event_id+"\",this.value,\"" + slotID + "\",\"" + slotLabel + "\",\"barcode_slot_"+value['project_id'] + "_" + value['record'] + "_" + value['event'] + "_" + (value['instance']+1)+"\");' /></span></div>";
             }
@@ -183,7 +183,8 @@ function retrieveSampleInfo(ajaxurl,project_id,event_id,barcode,slot_id,slot_lab
                     "<tr><td><h5>Expected Type</h5><br/>" + sampleData['planned_type'] + "</td><td><h5>EDC Type</h5><br/>" + sampleData['actual_type'] + "</td></tr>" +
                     "<tr><td><h5>Expected Collect Event</h5><br/>" + sampleData['planned_collect'] + "</td><td><h5>EDC Collect Event</h5><br/>" + sampleData['actual_collect'] + "</td></tr><tr><td>Issues</td><td><span><input id='sample_issue_1' type='checkbox' value='1' /><label for='sample_issue_1'>Empty</label></span><br/><span><input id='sample_issue_2' type='checkbox' value='2' /><label for='sample_issue_2'>Wrong Sample Type</label></span><br/><span><input id='sample_issue_3' type='checkbox' value='3' /><label for='sample_issue_3'>Sample Missing</label></span><br/><span><input id='sample_issue_4' type='checkbox' value='4' /><label for='sample_issue_4'>Damaged Sample</label></span><br/><span><input id='sample_issue_5' type='checkbox' value='5' /><label for='sample_issue_5'>Damaged Tube</label></span></td></tr><tr><td colspan='2'><label for='sample_issue_other'>Other Notes</label><textarea id='sample_issue_other' name='sample_issue_other'></textarea></td></tr>";
                 sampleTable += "<tr><td colspan='2' style='text-align:center;'><input type='button' onclick='saveSample(\""+ajaxurl+"\",\""+project_id+"\",\""+event_id+"\",\"" + barcode + "\",\"" + parent_id + "\",\"" + slot_label + "\",\"sample_issue_\",\"container_select\");$(\"#sample_info_container\").css(\"display\",\"none\");' value='Save Sample' /></td></tr>";
-                $('#' + parent_id).html("Part. ID: " + sampleData['participant_id'] + "<br/>Samp. ID: " + sampleData['sample_id'] + "<br/>Sample Type: " + sampleData['planned_type'] + "<br/>Collect Date: " + sampleData['collect_date']+ "<br/><input value='Checkout' type='button' id='sample_checkout_" + slot_label + "' onclick='checkoutSample(\""+ajaxurl+"\",\""+project_id+"\",\""+barcode+"\",\""+slot_id+"\");' />");
+                $('#' + parent_id).html("Part. ID: " + sampleData['participant_id'] + "<br/>Samp. ID: " + sampleData['sample_id'] + "<br/>Sample Type: " + sampleData['planned_type'] + "<br/>Collect Date: " + sampleData['collect_date']+ "<br/><input value='Checkout' type='button' id='sample_checkout_" + slot_label + "' onclick='checkoutSample(\""+ajaxurl+"\",\""+project_id+"\",\""+event_id+"\",\""+barcode+"\",\""+slot_id+"\",\""+slot_label+"\",\""+input_next+"\");' />");
+
                 sampleTable += "</table>";
                 $('#'+input_next).focus();
             }
@@ -238,7 +239,9 @@ function saveSample(ajaxurl,project_id,event_id,barcode,slot_id,slot_label,issue
     }
 }
 
-function checkoutSample(ajaxurl,project_id,barcode,slot_id) {
+function checkoutSample(ajaxurl,project_id,event_id,barcode,slot_id,slot_label,input_focus_id) {
+    let parent_id = "sample_slot_"+slot_id;
+
     $.ajax({
         url: ajaxurl,
         data: {
@@ -249,7 +252,10 @@ function checkoutSample(ajaxurl,project_id,barcode,slot_id) {
         },
         type: 'POST'
     }).done(function (html) {
-        console.log(html);
-
+        //console.log(html);
+        let result = JSON.parse(html);
+        if (result['removed']) {
+            $('#' + parent_id).html("<span class='scan_barcode'><label for='barcode_slot_" + slot_id + "'>Scan Barcode:</label><input class='barcode_text' type='text' id='barcode_slot_" + slot_id + "' oninput='saveSample(\""+ajaxurl+"\",\""+project_id+"\",\""+event_id+"\",this.value,\"" + slot_id + "\",\"" + slot_label + "\",\"sample_issue_\",\"container_select\");loadSample(\""+ajaxurl+"\",\""+project_id+"\",\""+event_id+"\",this.value,\"" + slot_id + "\",\"" + slot_label + "\",\""+input_focus_id+"\");' /></span>");
+        }
     });
 }
