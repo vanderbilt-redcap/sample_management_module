c7adb99a7d8bca147427a98fb80be743e79d53d9
moorejr5 <james.r.moore@vumc.org>
2025-02-23 22:57:10 -0600

    Added javascript function to copy div blocks for report definitions. Does not yet reset chosen values to reflect default instead of just copying values from existing blocks directly.

--- js/report_setup.js
+++ js/report_setup.js
@@ -27,3 +27,22 @@ window.loadProjectInfo = function(index, project_id, current_field, current_oper
         })
 }
 
+window.newDivBlock = function(parent_div,block_class) {
+    let blocks = document.querySelectorAll('.'+block_class);
+    let n = 0;
+    if (blocks.length > 0) {
+        let new_index = blocks.length;
+        let block = blocks[0].cloneNode(true);
+        while (n < block.childElementCount) {
+            let splits = block.children[n].id.split('_');
+            splits[splits.length-1] = new_index;
+            block.children[n].id = splits.join('_');
+            n++;
+        }
+        $('#'+parent_div).append(block);
+    }
+    else {
+        console.log('no items');
+    }
+}
+
